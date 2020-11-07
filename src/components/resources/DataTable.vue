5d242bcfd19597c6e316d9014d749b0d1211719a
Farid Hidayat <e.faridhidayat@gmail.com>
2020-11-08 02:03:12 +0700

    dynamic import slot from wrapped components

--- src/components/resources/DataTable.vue
+++ src/components/resources/DataTable.vue
@@ -113,6 +113,11 @@
         </q-btn>
       </q-td>
     </template>
+
+    <!-- Pass on all named slots -->
+    <slot v-for="slot in Object.keys($slots)" :name="slot" :slot="slot"/>
+    <!-- Pass on all scoped slots -->
+    <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope"><slot :name="slot" v-bind="scope"/></template>
   </q-table>
 </template>
 
