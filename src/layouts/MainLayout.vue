6a9a9c58b852fb177ac55670713d6a5f05a600c5
Farid Hidayat <e.faridhidayat@gmail.com>
2021-04-08 22:56:27 +0700

    topbar menu and logout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -56,11 +56,50 @@
             </q-badge>
             <q-tooltip>Notifications</q-tooltip>
           </q-btn>
-          <q-btn round flat>
+
+          <q-btn round flat to="/profile">
             <q-avatar size="26px">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
-            <q-tooltip>Account</q-tooltip>
+            <q-tooltip>Your Profile</q-tooltip>
+          </q-btn>
+          <q-btn flat round dense icon="more_vert">
+            <q-menu
+              transition-show="jump-down"
+              transition-hide="jump-up"
+            >
+              <q-list>
+                <q-item-label header>Settings</q-item-label>
+                <q-item clickable v-close-popup >
+                  <q-item-section avatar>
+                    <q-avatar icon="flag" color="primary" text-color="white" />
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Language</q-item-label>
+                    <q-item-label caption>English</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup >
+                  <q-item-section avatar>
+                    <q-avatar icon="more_horiz" color="primary" text-color="white" />
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Settings</q-item-label>
+                    <q-item-label caption>More settings</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-separator inset spaced />
+                <q-item clickable v-close-popup tabindex="0" @click="logout">
+                  <q-item-section avatar>
+                    <q-avatar icon="exit_to_app" color="negative" text-color="white" />
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Log Out</q-item-label>
+                    <q-item-label caption>Exit to app</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
           </q-btn>
         </div>
       </q-toolbar>
@@ -196,6 +235,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'MainLayout',
   data () {
@@ -227,6 +267,48 @@ export default {
         { icon: 'dashboard', text: 'Collage' },
         { icon: 'book', text: 'Photo book' }
       ]
+    }
+  },
+  computed: {
+    ...mapGetters({
+      profile: 'auth/user',
+      loggedIn: 'auth/loggedIn'
+    }),
+    name () {
+      return this.profile.first_name + ' ' + this.profile.last_name
+    },
+    username () {
+      return this.profile.username
+    }
+  },
+  methods: {
+    logout () {
+      this.$q.dialog({
+        title: 'Logout',
+        message: 'Are you sure to logout?',
+        ok: {
+          label: 'Logout',
+          color: 'negative',
+          flat: true
+        },
+        cancel: {
+          label: 'Cancel',
+          color: 'white',
+          textColor: 'black',
+          flat: true
+        },
+        persistent: true
+      }).onOk(() => {
+        this.$q.localStorage.clear()
+        this.$q.sessionStorage.clear()
+        this.$q.cookies.remove('authorization_token')
+        this.$router.push('/login')
+        this.$q.notify('You\'re logged out')
+      }).onCancel(() => {
+        // console.log('>>>> Cancel')
+      }).onDismiss(() => {
+        // console.log('I am triggered on both OK and Cancel')
+      })
     }
   }
 }
