ea7ad76ba5d545ed5119b9edb6987daf6838e358
Farid Hidayat <e.faridhidayat@gmail.com>
2021-04-11 16:46:36 +0700

    Resource component edit

--- src/pages/resources/edit.vue
+++ src/pages/resources/edit.vue
@@ -1,37 +1,220 @@
 <template>
   <q-page class="q-pa-md">
+    <q-ajax-bar
+      ref="loadingbar"
+      position="top"
+      skip-hijack
+    />
     <q-toolbar class="q-pb-md q-px-none">
       <q-breadcrumbs>
         <q-breadcrumbs-el :label="collectionName" :to="`/${collection}`" />
-        <q-breadcrumbs-el label="Update" />
+        <q-breadcrumbs-el label="Detail" />
       </q-breadcrumbs>
       <q-toolbar-title></q-toolbar-title>
     </q-toolbar>
-    <FormLayout
+    <div class="text-h5">Update {{collectionName}}</div>
+
+    <FormGenerator
+      ref="formGenerator"
       :collection="collection"
       :stateForm="stateForm"
+      :layout="layout"
+      :validation="validation"
       :id="id"
     />
+
+    <q-footer reveal elevated class="bg-white text-black">
+      <q-toolbar style="height: 64px">
+        <q-btn flat label="Cancel" :to="`/${collection}`" />
+        <q-space />
+        <q-btn icon="delete" flat color="negative" label="Delete" @click="confirmDelete(id)" />
+        <q-btn icon="check" class="q-ml-md bg-primary text-white" :loading="loading" color="secondary" label="Update" @click="submitUpdate" />
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
 <script>
-import FormLayout from 'components/form/FormLayout'
+import { mapState } from 'vuex'
+import { useStore } from 'vuex'
+import FormGenerator from 'components/form/FormGenerator'
+
 export default {
   components: {
-    FormLayout
+    FormGenerator
+  },
+  props: {
+    collection: {
+      type: String,
+      default: null
+    }
+  },
+  setup (props) {
+
+  },
+  provide () {
+    return {
+      form: this.form
+    }
+  },
+  mounted () {
+    const $store = useStore()
+    const { loadingbar } = this.$refs
+    loadingbar.start()
+    this.loading = true
+
+    $store
+    .dispatch(`${this.collection}/detail`, { id: this.id })
+    .then(response => {
+      const { data } = response
+      this.loading = false
+      loadingbar.stop()
+      const keys = Object.keys(this.form)
+      for (const i in keys) {
+        const key = keys[i]
+        this.form[key] = data[key]
+      }
+      this.$refs.formGenerator.setFormData(this.form)
+    })
+    .catch(error => {
+      this.loading = false
+      if (!error.response) return
+      const { data } = error.response
+      this.$q.dialog({
+        title: `${data.status}`,
+        message: `${data.message}`,
+        ok: {
+          flat: true
+        },
+        persistent: true
+      })
+    })
   },
   data () {
-    const { collection } = this.$route.params
     return {
-      collection,
       stateForm: 'update', // create, update, show
-      id: this.$route.params.id
+      id: this.$route.params.id,
+      isPwd: true,
+      loading: false
     }
   },
   methods: {
+    confirmDelete (id) {
+      this.$q.dialog({
+        title: 'Delete',
+        message: 'Are you sure to delete?',
+        ok: {
+          label: 'Delete',
+          color: 'negative',
+          flat: true
+        },
+        cancel: {
+          label: 'Cancel',
+          color: 'white',
+          textColor: 'black',
+          flat: true
+        },
+        persistent: true
+      }).onOk(() => {
+        const $store = useStore()
+        $store.dispatch(`${this.collection}/destroy`, {
+          type: id,
+          params: {}
+        }).then((response) => {
+          const { status, message } = response
+          this.$q.dialog({
+            title: `${status}`,
+            message: `${message}`,
+            ok: {
+              flat: true
+            },
+            persistent: true
+          }).onOk(() => {
+            this.$router.push(`/${this.collection}`)
+          })
+        }).catch(error => {
+          if (error.response) {
+            const { data } = error.response
+            this.$q.dialog({
+              title: `${data.status}`,
+              message: `${data.message}`,
+              ok: {
+                flat: true
+              },
+              persistent: true
+            })
+          }
+          this.loading = false
+        })
+      })
+    },
+
+    submitUpdate () {
+      const { formGenerator } = this.$refs
+      if (formGenerator.validateError()) return
+
+      const { loadingbar } = this.$refs
+      loadingbar.start()
+      this.loading = true
+
+      const payload = {
+        id: this.id,
+        data: formGenerator.form
+      }
+
+      this.$store
+      .dispatch(`${this.collection}/update`, payload)
+      .then((response) => {
+        const { status, message } = response
+        this.$q.dialog({
+          title: `${status}`,
+          message: `${message}`,
+          ok: {
+            flat: true
+          },
+          persistent: true
+        }).onOk(() => {
+          if (!this.submitAndCreate) {
+            this.$router.push(`/${this.collection}`)
+          }
+        }).finally(() => {
+          this.loading = false
+        })
+      })
+      .catch((error) => {
+        if (error.response) {
+          const { data } = error.response
+          this.$q.dialog({
+            title: `${data.status}`,
+            message: `${data.message}`,
+            ok: {
+              flat: true
+            },
+            persistent: true
+          })
+        }
+      })
+      .finally(() => {
+        loadingbar.stop()
+        this.loading = false
+      })
+    },
   },
   computed: {
+    ...mapState({
+      validation (state, getters) {
+        return getters[`${this.collection}/validation`]
+      },
+      form (state, getters) {
+        return getters[`${this.collection}/form`]
+      },
+      layout (state, getters) {
+        return getters[`${this.collection}/layout`]
+      }
+    }),
+    readonly () {
+      return this.stateForm === 'show'
+    },
     collectionName () {
       const words = this.collection.split('_')
       const titles = []
@@ -40,7 +223,14 @@ export default {
         titles.push(word.charAt(0).toUpperCase() + word.slice(1))
       }
       return titles.join(' ')
-    }
+    },
+    titlePage () {
+      let title = ''
+      if (this.stateForm === 'create') title = 'Create New'
+      if (this.stateForm === 'show') title = 'Detail'
+      if (this.stateForm === 'update') title = 'Update'
+      return title
+    },
   }
 }
 </script>
