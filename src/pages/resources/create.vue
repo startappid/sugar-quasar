ea8d576c2861e720fd5a3373654e3e496bcdc6c5
Farid Hidayat <e.faridhidayat@gmail.com>
2021-04-11 17:26:29 +0700

    Resources create form

--- src/pages/resources/create.vue
+++ src/pages/resources/create.vue
@@ -1,24 +1,52 @@
 <template>
   <q-page class="q-pa-md">
+    <q-ajax-bar
+      ref="loadingbar"
+      position="top"
+      skip-hijack
+    />
     <q-toolbar class="q-pb-md q-px-none">
       <q-breadcrumbs>
         <q-breadcrumbs-el :label="collectionName" :to="`/${collection}`" />
-        <q-breadcrumbs-el label="Create" />
+        <q-breadcrumbs-el label="Detail" />
       </q-breadcrumbs>
       <q-toolbar-title></q-toolbar-title>
     </q-toolbar>
-    <FormLayout
+    <div class="text-h5">Create {{collectionName}}</div>
+
+    <FormGenerator
+      ref="formGenerator"
       :collection="collection"
       :stateForm="stateForm"
+      :layout="layout"
+      :validation="validation"
     />
+
+    <q-footer reveal elevated class="bg-white text-black">
+      <q-toolbar style="height: 64px">
+        <q-toggle v-model="submitAndCreate" label="Submit and create new" />
+        <q-space />
+        <q-btn flat label="Cancel" :to="`/${collection}`" />
+        <q-btn
+          icon="check"
+          class="q-ml-md bg-primary text-white"
+          color="secondary"
+          label="Create"
+          :loading="loading"
+          @click="submit"
+        />
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
 <script>
-import FormLayout from 'components/form/FormLayout'
+import { mapState } from 'vuex'
+import FormGenerator from 'components/form/FormGenerator'
+
 export default {
   components: {
-    FormLayout
+    FormGenerator
   },
   props: {
     collection: {
@@ -26,14 +54,96 @@ export default {
       default: null
     }
   },
+  setup (props) {
+
+  },
+  provide () {
+    return {
+      form: this.form
+    }
+  },
+  mounted () {
+
+  },
   data () {
     return {
-      stateForm: 'create' // create, update, show
+      stateForm: 'create', // create, update, show
+      isPwd: true,
+      loading: false,
+      submitAndCreate: false
     }
   },
   methods: {
+    submit () {
+      const { formGenerator } = this.$refs
+      if (formGenerator.validateError()) return
+
+      const { loadingbar } = this.$refs
+      loadingbar.start()
+      this.loading = true
+
+      const payload = {
+        data: formGenerator.form
+      }
+
+      this.$store
+      .dispatch(`${this.collection}/create`, payload)
+      .then((response) => {
+        const { status } = response
+        this.$q.dialog({
+          title: `${status}`,
+          message: 'Data created',
+          ok: {
+            flat: true
+          },
+          persistent: true
+        }).onOk(() => {
+          if (!this.submitAndCreate) {
+            this.$router.push(`/${this.collection}`)
+          } else {
+            const keys = Object.keys(this.form)
+            for (const i in keys) {
+              const key = keys[i]
+              this.form[key] = null
+            }
+            this.$refs.formGenerator.resetForm()
+          }
+        }).finally(() => {
+          this.loading = false
+        })
+      }).catch((error) => {
+        if (error.response) {
+          const { data } = error.response
+          this.$q.dialog({
+            title: `${data.status}`,
+            message: `${data.message}`,
+            ok: {
+              flat: true
+            },
+            persistent: true
+          })
+        }
+      }).finally(() => {
+        loadingbar.stop()
+        this.loading = false
+      })
+    },
   },
   computed: {
+    ...mapState({
+      validation (state, getters) {
+        return getters[`${this.collection}/validation`]
+      },
+      form (state, getters) {
+        return getters[`${this.collection}/form`]
+      },
+      layout (state, getters) {
+        return getters[`${this.collection}/layout`]
+      }
+    }),
+    readonly () {
+      return this.stateForm === 'show'
+    },
     collectionName () {
       const words = this.collection.split('_')
       const titles = []
@@ -42,7 +152,13 @@ export default {
         titles.push(word.charAt(0).toUpperCase() + word.slice(1))
       }
       return titles.join(' ')
-    }
+    },
+    titlePage () {
+      let title = ''
+      if (this.stateForm === 'create') title = 'Create New'
+      if (this.stateForm === 'show') title = 'Detail'
+      return title
+    },
   }
 }
 </script>
