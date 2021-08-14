1f1cd179191dd6ae7bf3615da15dd7ad0e08b26e
Farid Hidayat <e.faridhidayat@gmail.com>
2021-08-15 00:32:41 +0700

    sysparam state

--- src/store/sysparam/state.js
+++ src/store/sysparam/state.js
@@ -1,9 +1,77 @@
 import state from '../resources/state'
-export const collection = 'sysparam'
-export const columns = []
-export const form = {}
+
+export const collection = 'sysparams'
+export const params = {
+  // 'orderby[name]': 'asc'
+}
+export const form = {
+  group: null,
+  key: null,
+  value: null,
+  data: null,
+  order: 0,
+  status: 'active',
+}
+export const resetValue = {...form}
+
+export const columns = [
+  {
+    name: 'group',
+    required: true,
+    label: 'Group',
+    align: 'left',
+    field: 'group',
+    format: (val) => val,
+    sortable: true
+  },
+  {
+    name: 'key',
+    required: true,
+    label: 'Key',
+    align: 'left',
+    field: 'key',
+    format: (val) => val,
+    sortable: true
+  },
+  {
+    name: 'value',
+    required: true,
+    label: 'Value',
+    align: 'left',
+    field: 'value',
+    format: (val) => val,
+    sortable: true
+  },
+  {
+    name: 'order',
+    required: true,
+    label: 'Order',
+    align: 'left',
+    field: 'order',
+    format: (val) => val,
+    sortable: true
+  },
+  {
+    name: 'status',
+    required: true,
+    label: 'Status',
+    align: 'left',
+    field: 'status',
+    format: (val) => val,
+    sortable: true
+  },
+
+  // Always give this columns as default
+  {
+    name: 'action',
+    label: null,
+    field: 'key'
+  }
+]
+
 export const layout = []
 export const validation = {}
+export const data = []
 export const route = {}
 
 export default function () {
@@ -11,9 +79,11 @@ export default function () {
     ...state(),
     // Collection name
     collection,
+    params,
     // Datatable config
     columns,
     form,
+    resetValue,
     layout,
     validation,
     route
